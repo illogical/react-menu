0b8f589d169f54b285e8d89f3942c72d9714ca39
Matt Moore <thewmoore@gmail.com>
2019-04-27 01:43:51 -0400

    Fixed losing active menu item after navigating to submenu.

--- src/menu/leftNav.tsx
+++ src/menu/leftNav.tsx
@@ -37,7 +37,7 @@ export const LeftNav: React.FunctionComponent = ({ children }) => {
   };
 
   const onSubmenuTitleClick = () => {
-    setMenuConfig(hubMenuConfig);
+    setMenuConfig(setActiveByLocation(hubMenuConfig, window.location.href));
   };
 
   const onMenuItemClick = (config: IMenuConfig, activeItemHref?: string) => {
@@ -99,18 +99,19 @@ const findMenuContainingPath = (
   config: IMenuConfig,
   location: string
 ): IMenuConfig => {
+  let menuContainingLink: IMenuConfig = config;
+  // check if any submenus' urls match the current browser url
+  config.items.map(menuItem => {
+    if (menuItem.submenu && containsPath(menuItem.submenu, location)) {
+      menuContainingLink = findMenuContainingPath(menuItem.submenu, location);
+    }
+  });
+
   if (containsPath(config, location)) {
     return setActiveByLocation(config, location);
   }
 
-  let foundInConfig: IMenuConfig = config;
-  config.items.map(menuItem => {
-    if (menuItem.submenu && containsPath(menuItem.submenu, location)) {
-      foundInConfig = setActiveByLocation(menuItem.submenu, location);
-    }
-  });
-
-  return foundInConfig;
+  return menuContainingLink;
 };
 
 const containsPath = (config: IMenuConfig, location: string): boolean => {
